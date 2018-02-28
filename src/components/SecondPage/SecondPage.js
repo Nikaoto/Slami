20553317032590d1967cb8ad5523dfed87ccdec2
Nikaoto <nikaoto@gmail.com>
2018-02-28 20:25:18 +0400

    add SecondPage compo

---
+++ src/components/SecondPage/SecondPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+export default class SecondPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div className="row">
+      </div>
+    )
+  }
+}
