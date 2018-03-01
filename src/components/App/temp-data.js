e40130e490f2b07f83e518d1c3d628beb9e7dc62
Nikaoto <nikaoto@gmail.com>
2018-03-01 23:51:40 +0400

    add temp data for Second Page testing

---
+++ src/components/App/temp-data.js
@@ -0,0 +1,16 @@
+const data = [
+  {key: 0, url: "http://www.primrose-bakery.co.uk/shop/content/images/thumbs/0002213_vegan-chocolate-layer-cake.jpeg", title: "Vegan Chocolate Layer Cake", source: "http://www.primrose-bakery.co.uk/shop/vegan-chocolate-layer-cake", num: 1},
+  {key: 1, url: "https://spottedhorsechick.files.wordpress.com/2010/01/dogs-044.jpg", title: "Gracie, a Scattergun Dog | Colorful Beauty", source: "https://spottedhorsechick.wordpress.com/2010/01/10/gracie-a-scattergun-dog/", num: 2},
+  {key: 41, url: "https://spottedhorsechick.files.wordpress.com/2010/01/dogs-044.jpg", title: "Gracie, a Scattergun Dog | Colorful Beauty", source: "https://spottedhorsechick.wordpress.com/2010/01/10/gracie-a-scattergun-dog/", num: 2},
+{key: 10, url: "http://www.primrose-bakery.co.uk/shop/content/images/thumbs/0002213_vegan-chocolate-layer-cake.jpeg", title: "Vegan Chocolate Layer Cake", source: "http://www.primrose-bakery.co.uk/shop/vegan-chocolate-layer-cake", num: 1},
+  {key: 2, url: "https://www.laoistoday.ie/wp-content/uploads/2017/11/Baby.jpg", title: "Baby boom in Laois continues - Laois Today", source: "https://www.laoistoday.ie/2017/11/30/baby-boom-laois-continues/", num: 3},
+{key: 60, url: "http://www.primrose-bakery.co.uk/shop/content/images/thumbs/0002213_vegan-chocolate-layer-cake.jpeg", title: "Vegan Chocolate Layer Cake", source: "http://www.primrose-bakery.co.uk/shop/vegan-chocolate-layer-cake", num: 1},
+{key: 70, url: "http://www.primrose-bakery.co.uk/shop/content/images/thumbs/0002213_vegan-chocolate-layer-cake.jpeg", title: "Vegan Chocolate Layer Cake", source: "http://www.primrose-bakery.co.uk/shop/vegan-chocolate-layer-cake", num: 1},
+  {key: 52, url: "https://www.laoistoday.ie/wp-content/uploads/2017/11/Baby.jpg", title: "Baby boom in Laois continues - Laois Today", source: "https://www.laoistoday.ie/2017/11/30/baby-boom-laois-continues/", num: 3},
+  {key: 61, url: "https://spottedhorsechick.files.wordpress.com/2010/01/dogs-044.jpg", title: "Gracie, a Scattergun Dog | Colorful Beauty", source: "https://spottedhorsechick.wordpress.com/2010/01/10/gracie-a-scattergun-dog/", num: 2},
+  {key: 71, url: "https://spottedhorsechick.files.wordpress.com/2010/01/dogs-044.jpg", title: "Gracie, a Scattergun Dog | Colorful Beauty", source: "https://spottedhorsechick.wordpress.com/2010/01/10/gracie-a-scattergun-dog/", num: 2},
+  {key: 62, url: "https://www.laoistoday.ie/wp-content/uploads/2017/11/Baby.jpg", title: "Baby boom in Laois continues - Laois Today", source: "https://www.laoistoday.ie/2017/11/30/baby-boom-laois-continues/", num: 3},
+  {key: 72, url: "https://www.laoistoday.ie/wp-content/uploads/2017/11/Baby.jpg", title: "Baby boom in Laois continues - Laois Today", source: "https://www.laoistoday.ie/2017/11/30/baby-boom-laois-continues/", num: 3},
+]
+
+export default data
