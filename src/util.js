9407fc807eff52f87463a57eee521a5371913e6a
Nikaoto <nikaoto@gmail.com>
2018-03-03 23:04:59 +0400

    add generateVideo to util

--- src/util.js
+++ src/util.js
@@ -20,3 +20,41 @@ export function getImportantWords(paragraph) {
     return importantWords
   }
 }
+
+export function generateVideo(slides, context, canvas, onFinish) {
+  const fps = 10
+
+  // Geo font
+  //const Kartuli = new FontFace('Kartuli', 'url(fonts/NotoSansGeorgian-Regular.ttf)');
+
+  const Whammy = require("./whammy")
+  const video = new Whammy.Video(fps)
+
+  // TODO: if fails check with canvas.width and height (pass canvas as arg)
+  const clearCanvas = (ctx) => ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+  slides.forEach((slide, index) => {
+
+    const img = new Image()
+    img.crossOrigin = "use-credentials"
+    img.onload = () => {
+      context.drawImage(img, 0, 0, canvas.width, canvas.height)
+      //drawText
+      console.log("add frame")
+      video.add(context)
+      clearCanvas(context)
+
+      // Check if finished
+      if (index >= slides.length) {
+/*        console.log("compile")
+
+        video.compile(false, (output) => {
+          console.log(output)
+          onFinish(output)
+        })*/
+      }
+    }
+    img.src = slide.url //TODO: check if I have to use FileReader for local images
+
+  })
+}
