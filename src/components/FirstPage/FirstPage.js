f11de82d1fc9bb6724d28ab9cef37868b1077fa8
Nikaoto <nikaoto@gmail.com>
2018-03-16 15:01:29 +0400

    fix NPE crash

--- src/components/FirstPage/FirstPage.js
+++ src/components/FirstPage/FirstPage.js
@@ -123,10 +123,12 @@ class FirstPage extends Component {
           return m
         })
         .sort((a, b) => a.num - b.num)
-          
-      finalMedia[0].selected = true
 
-      this.props.onNextPage(finalMedia)
+      if (finalMedia[0]) {
+        finalMedia[0].selected = true
+
+        this.props.onNextPage(finalMedia)
+      }
     }
   }
 
