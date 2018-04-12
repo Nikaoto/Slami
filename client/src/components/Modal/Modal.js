febe027ffc29343ae5fe75b15fac27af071022cb
Nikaoto <nikaoto@gmail.com>
2018-04-12 19:26:44 +0400

    fix Modal styles; improve Modal compo

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react'
-
+import Button from "../Button"
+import "./Modal.css"
 
 class Modal extends Component {
   constructor(props) {
     super(props)
-    this.state = { visible: false }
+
+    this.onClose = this.onClose.bind(this)
+  }
+
+  onClose() {
+    if (this.props.onClose) {
+      this.props.onClose()
+    }
   }
 
   render() {
-    if (this.state.visible) {
-      return <div/>
-    }
-
     return (
-      <div>MODAL</div>
+
+      <div className="c-modal">
+
+        <div className="c-modal-content">
+
+          <div className="c-modal-header">
+            <span className="c-close" onClick={this.onClose}>&times;</span>
+            {this.props.header}
+          </div>
+
+          <div className="c-modal-body">
+            {this.props.body}
+          </div>
+
+          <div className="c-modal-footer">
+            {this.props.footer}
+          </div>
+
+        </div>
+
+      </div>
     )
   }
+}
+
+const styles = {
 }
 
 export default Modal
