a42319c85efbda740b00bd4b31143670feb2275f
Nikaoto <nikaoto@gmail.com>
2018-04-20 13:17:15 +0400

    minor fixes in server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use(express.static(`${__dirname}/client/build`))
+//app.use(express.static(`${__dirname}/client/build`))
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "/client/build", "index.html"))
