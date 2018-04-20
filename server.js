2e6bcdf1475dd5735dae340a55dbfe8c6798aff2
Nikaoto <nikaoto@gmail.com>
2018-04-20 13:26:13 +0400

    fix server

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-//app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(__dirname + "/client/build"))
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "/client/build", "index.html"))
-})
+})*/
 
 /*app.get("/", (req, res) => {
   res.end("Hi :^)")
